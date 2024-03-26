8ee282ff8b24a1ebc2ccec919a5ca5c4305991ca
unknown <kalidashodekare14@gmail.com>
2024-03-26 14:03:54 +0600

    Book Vibe Project

---
+++ src/Components/Book/Book.jsx
@@ -0,0 +1,27 @@
+import { FaRegStar } from "react-icons/fa";
+
+const Book = ({ book }) => {
+    const { id, image, tags, bookName, publisher, category, rating } = book
+    return (
+        <div className="card w-96 bg-base-100 border p-3">
+            <img className="h-80 bg-[#F3F3F3] rounded-2xl p-10" src={image} alt="Shoes" />
+            <div className="card-body">
+                <div className="flex mr-3 font-semibold text-[18px]">
+                    <p className="text-[#23BE0A] ">{tags[0]}</p>
+                    <p className="text-[#23BE0A] ">{tags[1]}</p>
+                </div>
+                <h2 className="card-title">{bookName}</h2>
+                <p>By: {publisher}</p>
+                <div className="flex mr-3">
+                    <p>{category}</p>
+                    <div className="flex items-center space-x-3">
+                        <p className="">{rating}</p>
+                        <FaRegStar />
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Book;
---
+++ src/Components/Books/Books.jsx
@@ -0,0 +1,25 @@
+import { useEffect, useState } from "react";
+import Book from "../Book/Book";
+
+const Books = () => {
+    const [books, setBooks] = useState([])
+
+    useEffect(()=>{
+        fetch('books.json')
+        .then(res => res.json())
+        .then(data => setBooks(data));
+    },[])
+
+    return (
+        <div>
+            <h2 className="text-center text-4xl font-bold my-20">Book</h2>
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 my-5">
+                {
+                    books.map(book => <Book key={book.id} book={book}></Book>)
+                }
+            </div>
+        </div>
+    );
+};
+
+export default Books;
